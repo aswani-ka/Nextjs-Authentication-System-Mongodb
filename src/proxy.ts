96e827488997fa3594f721e922a0b04d9460c5ab
ASWANI K A <aswanika05@gmail.com>
2025-12-05 18:24:00 +0530

    feat: add auth middleware to protect private routes

---
+++ src/proxy.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function proxy(request: NextRequest) {
+    const path = request.nextUrl.pathname
+    const ispublicPath = path === '/login' || path === '/signup'
+    const token = request.cookies.get("token")?.value || ""
+    if(ispublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+    if(!ispublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+
+}
+ 
+
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/profile',
+    '/login',
+    '/signup',
+    '/verifyemail'
+  ]
+}
